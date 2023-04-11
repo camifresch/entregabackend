bb920d4d569a363bfae689f0cfede5501f7db2ae
Cami Fresch <cami.fresch@hotmail.com>
2023-04-11 17:28:47 -0300

    arreglos

--- segundaentrega/ProductManager.js
+++ segundaentrega/ProductManager.js
@@ -43,7 +43,14 @@ class ProductManager {
     }
 
     validProduct(product) {
-        return !this.products.find(pr => product.code == pr.code) && product.title && product.description && product.price && product.thumbnail && product.stock
+        return (
+        !this.products.find((pr) => product.code == pr.code) && 
+        product.title && 
+        product.description && 
+        product.price && 
+        product.thumbnail && 
+        product.stock
+        );
     }
 
     getProducts = async () => {
@@ -52,14 +59,19 @@ class ProductManager {
     }
 
     getProductById = async (productId) => {
-        const products = await fs.promises.readFile(this.path, 'utf-8')
-        return JSON.parse(products).find(product => product.id === productId);
+        const products = await fs.promises.readFile(this.path, 'utf-8');
+        const parsedProducts = JSON.parse(products);
+        const product = parsedProducts.find((p) => p.id === parseInt(productId))
+        if (!product) {
+            throw new Error(`Product with id ${productId} not found`);
+        }
+        return product;
     };
 
     updateProduct = async(productId, fieldToUpdate, newValue) => {
         const products = await fs.promises.readFile(this.path, 'utf-8');
         const transfProd = JSON.parse(products); 
-        const index = transfProd.findIndex(product => product.id === productId);
+        const index = transfProd.findIndex((product) => product.id === productId);
         
         transfProd[index][fieldToUpdate] = newValue; 
         
@@ -67,21 +79,17 @@ class ProductManager {
     }
 
     deleteProductById = async (productId) => {
-        const products = await fs.promises.readFile(this.path, 'utf-8')
-        const updatedProducts = JSON.parse(products).filter(product => product.id != productId);
-        console.log("productos sin eliminar: ", updatedProducts);
-        console.log("productos: ", products);
+        const products = await fs.promises.readFile(this.path, 'utf-8');
+        const parsedProducts = JSON.parse(products);
+        const updatedProducts = parsedProducts.filter(
+            (p) => p.id !== parseInt(productId)
+        );
+        this.products = updatedProducts; // update this.products array
         await fs.promises.writeFile(this.path, JSON.stringify(updatedProducts));
+        return updatedProducts;
     };
 }
-
-let productManager = new ProductManager("./productos.json")
-
-console.log("buscando productos")
-productManager.getProducts().then( (products) => {
-    console.log("productos: ", products);
-    console.log("termina buscando productos");
-}).catch(err => console.log("error buscando productos"));
+let productManager = new ProductManager('./productos.json');
 
 productManager.addProduct({
     title: 'producto prueba',
@@ -90,25 +98,83 @@ productManager.addProduct({
     thumbnail:'Sin imagen',
     code:'abc123',
     stock:25
-})
+});
 
-productManager.getProducts().then(products => {
-    console.log("productos: ", products);
-    console.log("termina buscando productos");
-}).catch(err => console.log("error buscando productos"));
+productManager.addProduct({
+    title: 'producto prueba 2',
+    description:'Este es un producto prueba',
+    price:'200',
+    thumbnail:'Sin imagen',
+    code:'abc123',
+    stock:25
+});
+
+productManager.addProduct({
+    title: 'producto prueba 3',
+    description:'Este es un producto prueba',
+    price:'200',
+    thumbnail:'Sin imagen',
+    code:'abc123',
+    stock:25
+});
+
+productManager.addProduct({
+    title: 'producto prueba 4',
+    description:'Este es un producto prueba',
+    price:'200',
+    thumbnail:'Sin imagen',
+    code:'abc123',
+    stock:25
+});
+
+productManager.addProduct({
+    title: 'producto prueba 5',
+    description:'Este es un producto prueba',
+    price:'200',
+    thumbnail:'Sin imagen',
+    code:'abc123',
+    stock:25
+});
+
+productManager.addProduct({
+    title: 'producto prueba 6',
+    description:'Este es un producto prueba',
+    price:'200',
+    thumbnail:'Sin imagen',
+    code:'abc123',
+    stock:25
+});
+
+productManager
+.getProducts()
+.then((products) => {
+    console.log('productos: ', products);
+    console.log('termina buscando productos');
+})
+    .catch((err) => console.log('error buscando productos'));
 
 productManager.getProductById(1).then(product => {
     console.log("producto by id 1: ", product);
 }).catch(err => console.log("error buscando producto by id"));
 
-productManager.getProductById(1000).then(product => {
-    if (product) {
-        console.log("producto by id 1000: ", product);
-    } else {
-        console.log("producto by id 1000 no encontrado");
-    }
-}).catch(err => console.log("error buscando producto by id 1000"));
+productManager
+.getProductById(1)
+.then((product) => {
+        console.log('producto by id 1: ', product);
+}).catch((err) => console.log('error buscando producto by id'));
 
-productManager.updateProduct(1, "description", "campo actualizado");
+productManager
+.deleteProductById(1)
+.then((updatedProducts) => {
+    console.log(`producto(s) borrado(s): ${JSON.stringify(updatedProducts)}`);
+})
+.catch((err) => console.log('error borrando producto by id'));
 
-productManager.deleteProductById(1);
+productManager
+.getProducts()
+.then((products) => {
+    console.log('productos: ', products);
+    // console.log('termina buscando productos');
+})
+.catch((err) => console.log('error buscando productos'));
+
