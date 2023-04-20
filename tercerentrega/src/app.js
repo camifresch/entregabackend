7387fc3967468ca3478b8e14509cd8030709a217
Cami Fresch <cami.fresch@hotmail.com>
2023-04-20 17:54:05 -0300

    ultimo

--- tercerentrega/src/app.js
+++ tercerentrega/src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const {ProductManager} = require('./components/ProductManager');
+const { ProductManager } = require('./components/ProductManager');
 
-const PUERTO = 8030;
+const PUERTO = 8080;
 
 const server = express();
 
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 
 //creo el productManager
 const manager = new ProductManager();
@@ -13,7 +13,7 @@ const getProducts = manager.getProducts();
 
 server.get('/products', async (req, res) => {
     let limit = parseInt(req.query.limit);
-if(!limit) return res.send(await getProducts);
+    if (!limit) return res.send(await getProducts);
 
     let allProducts = await getProducts;
     let productLimit = allProducts.slice(0, limit)
@@ -24,7 +24,15 @@ server.get('/products/:id', async (req, res) => {
     let id = parseInt(req.params.id);
     let allProducts = await getProducts;
     let productById = allProducts.find(product => product.id === id);
-    res.send(productById);
+    if (productById) {
+        res.send(productById);
+    } else {
+        let error = {
+            code: 'not_found',
+            message: 'El producto con id ' + id + ' no existe'
+        };
+        res.status(404).send(error);
+    }
 });
 
 server.listen(PUERTO, () => {
